ab8e2971637e3495a4462d45e5e577a245b7a69d
Preeti Raj <satendra11kum@gmail.com>
2024-06-22 00:44:56 +0530

    Leading Page Complete

--- lingo/app/(marketing)/footer.tsx
+++ lingo/app/(marketing)/footer.tsx
@@ -1,7 +1,31 @@
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+
 export const Footer = () => {
   return (
     <footer className="hidden lg:block h-20 w-full border-t-2 border-slate-200 p-2">
-      Footer
+      <div className="max-w-screen-lg mx-auto flex items-center justify-center h-full">
+        <Button size="lg" variant="ghost" className="w-full">
+          <Image src="/hr.svg" alt="Croatian" height={32} width={40} className="mr-4 rounded-md"/>
+          Croatian
+        </Button>
+        <Button size="lg" variant="ghost" className="w-full">
+          <Image src="/es.svg" alt="Croatian" height={32} width={40} className="mr-4 rounded-md"/>
+          Spainsh
+        </Button>
+        <Button size="lg" variant="ghost" className="w-full">
+          <Image src="/fr.svg" alt="Croatian" height={32} width={40} className="mr-4 rounded-md"/>
+          French
+        </Button>
+        <Button size="lg" variant="ghost" className="w-full">
+          <Image src="/it.svg" alt="Croatian" height={32} width={40} className="mr-4 rounded-md"/>
+          Italian
+        </Button>
+        <Button size="lg" variant="ghost" className="w-full">
+          <Image src="/jp.svg" alt="Croatian" height={32} width={40} className="mr-4 rounded-md"/>
+          Japanish
+        </Button>
+      </div>
     </footer>
   );
 };
