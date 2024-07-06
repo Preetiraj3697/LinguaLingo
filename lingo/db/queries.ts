c39c6cad840e710999b78c5b1ee6014d1dc96a04
Preeti Raj <satendra11kum@gmail.com>
2024-07-07 01:00:36 +0530

    quests & leaderboard page

--- lingo/db/queries.ts
+++ lingo/db/queries.ts
@@ -34,17 +34,16 @@ export const getUnits = cache(async () => {
   if (!userId || !userProgress?.activeCourseId) {
     return [];
   }
-  
-  
+
   const data = await db.query.units.findMany({
-    orderBy:(units,{asc}) =>[asc(units.order)],
+    orderBy: (units, { asc }) => [asc(units.order)],
     where: eq(units.courseId, userProgress.activeCourseId),
     with: {
       lessons: {
-        orderBy:(lessons,{asc}) => [asc(lessons.order)],
+        orderBy: (lessons, { asc }) => [asc(lessons.order)],
         with: {
           challenges: {
-            orderBy: (challenges,{asc}) => [asc(challenges.order)],
+            orderBy: (challenges, { asc }) => [asc(challenges.order)],
             with: {
               challengeProgress: {
                 where: eq(challengeProgress.userId, userId),
@@ -128,7 +127,6 @@ export const getCourseProgress = cache(async () => {
   const firstUncompletedLesson = unitsInActiveCourse
     .flatMap((unit) => unit.lessons)
     .find((lesson) => {
-
       return lesson.challenges.some((challenge) => {
         return (
           !challenge.challengeProgress ||
@@ -177,7 +175,6 @@ export const getLesson = cache(async (id?: number) => {
   }
 
   const normalizedChallenges = data.challenges.map((challenge) => {
-
     const completed =
       challenge.challengeProgress &&
       challenge.challengeProgress.length > 0 &&
@@ -226,4 +223,20 @@ export const getUserSubscription = cache(async () => {
     ...data,
     isActive: !!isActive,
   };
+});
+
+export const getTopTenUsers = cache(async () => {
+  const { userId } = await auth();
+  if (!userId) return [];
+  const data = await db.query.userProgress.findMany({
+    orderBy: (userProgress, { desc }) => [desc(userProgress.points)],
+    limit: 10,
+    columns: {
+      userId: true,
+      userName: true,
+      userImageSrc: true,
+      points: true,
+    },
+  });
+  return data;
 });
