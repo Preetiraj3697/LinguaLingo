6992b0bc2963a5fe406d4790d902a64ef8ace2ef
Preeti Raj <satendra11kum@gmail.com>
2024-07-08 02:29:09 +0530

    fix errors

--- lingo/lib/admin.ts
+++ lingo/lib/admin.ts
@@ -1,5 +1,7 @@
 import { auth } from "@clerk/nextjs/server";
 
+// "user_2iCVK4jHSONIE3GH6UpLyvelFJh"
+
 export const getIsAdmin = () => {
   const { userId } = auth();
   const adminIds = "user_2iCVK4jHSONIE3GH6UpLyvelFJh".split(", "); // stored in .env.local file as string separated by comma(,) and space( )
