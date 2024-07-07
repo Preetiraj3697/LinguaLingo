b0da9125e1908894c759a4f990fc45fc706a91f9
Preeti Raj <satendra11kum@gmail.com>
2024-07-08 01:49:20 +0530

    admin dashboard

---
+++ lingo/app/api/courses/route.ts
@@ -0,0 +1,31 @@
+import db from "@/db/drizzle";
+import { NextResponse, type NextRequest } from "next/server";
+import { courses } from "@/db/schema";
+import { getIsAdmin } from "@/lib/admin";
+
+export const GET = async () =>
+{
+    const isAdmin = getIsAdmin();
+    if(!isAdmin){
+        return new NextResponse("Unauthorized", { status: 401 });
+    }
+
+    const data = await db.query.courses.findMany();
+
+    return NextResponse.json(data);
+}
+
+export const POST = async (req: NextRequest) =>
+    {
+        const isAdmin = getIsAdmin();
+        if(!isAdmin){
+            return new NextResponse("Unauthorized", { status: 401 });
+        }
+    
+    const body = await req.json();
+    const data = await db.insert(courses).values({
+        ...body,
+    }).returning();
+    
+        return NextResponse.json(data[0]);
+    }
